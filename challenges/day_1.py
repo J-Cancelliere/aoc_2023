e1d2a72cbaaa8e71e09a42b8a326b8378684f0b4
Joseph Cancelliere <CancelliereJM@gmail.com>
2023-12-01 13:15:43 +0000

    Completed day 1, part 1. All tests passing

--- challenges/day_1.py
+++ challenges/day_1.py
@@ -1,19 +1,31 @@
 from aocd.models import Puzzle
 
 def retrieve_challenge_input():
-    pass
+    data = Puzzle(year=2023, day=1)
+    return data.input_data.split("\n")
 
-def extract_calibration_values():
-    pass
+def extract_calibration_values(line):
+    line_len = len(line)
+    calibration = {}
+    for i in range(line_len):
+        front = i
+        back = (i * -1) - 1 if i > 0 else -1
+        if line[front].isnumeric() and not calibration.get("front"):
+            calibration["front"] = line[front]
+        if line[back].isnumeric() and not calibration.get("back"):
+            calibration["back"] = line[back]
+    return int(calibration["front"] + calibration["back"])
 
-def collect_all_values():
-    pass
+def collect_all_values(data):
+    return [extract_calibration_values(datum) for datum in data]
 
-def sum_calibration_values():
-    pass
+def sum_calibration_values(calibration_list):
+    return sum(calibration_list)
 
 def main():
-    pass
+    data = retrieve_challenge_input()
+    calibration_list = collect_all_values(data)
+    return sum_calibration_values(calibration_list)
 
 if __name__ == "__main__":
     print(main())
