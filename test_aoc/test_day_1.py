5a0103afb825e56bde8c826aac44501efb6ec8f8
Joseph Cancelliere <CancelliereJM@gmail.com>
2023-12-01 13:22:18 +0000

    Writing tests for day 1, part 2

--- test_aoc/test_day_1.py
+++ test_aoc/test_day_1.py
@@ -35,3 +35,25 @@ def test_sum_function():
     Then they return 15 '''
     result = day_1.sum_calibration_values([1,2,3,4,5])
     assert result == 15
+
+# Tests for part 2
+def test_convert_many_words_to_numeric():
+    ''' Given a string 4nineeightseven2
+    When passed to the conversion function
+    Then it returns 49872'''
+    result = day_1.convert_words_to_numeric("4nineeightseven2")
+    assert result == "49872"
+
+def test_convert_2_words_to_numeric():
+    ''' Given a string abcone2threexyz
+    When passed to the conversion function
+    Then it returns abc123xyz'''
+    result = day_1.convert_words_to_numeric("abcone2threexyz")
+    assert result == "abc123xyz"
+
+def test_convert_1_word_to_numeric():
+    ''' Given a string 7pqrstsixteen
+    When passed to the conversion function
+    Then it returns 7pqrst6teen'''
+    result = day_1.convert_words_to_numeric("7pqrstsixteen")
+    assert result == "7pqrst6teen"
