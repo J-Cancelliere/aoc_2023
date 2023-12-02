bba85afd97c0ae886c33173e6a1267d18afbae94
Joseph Cancelliere <CancelliereJM@gmail.com>
2023-12-02 06:04:49 +0000

    Completed day 2 part 1

--- challenges/day_2.py
+++ challenges/day_2.py
@@ -1,16 +1,57 @@
 from aocd.models import Puzzle
 
+LIMITS = {"red": 12,
+          "green": 13,
+          "blue": 14}
+
 def retrieve_challenge_input():
-    pass
+    data = Puzzle(year=2023, day=2)
+    return data.input_data
 
-def parse_data_part_1():
-    pass
+def parse_data_part_1(data):
+    data_list = data.split('\n')
+    data_dict = {}
+    for line in data_list:
+        # Find colon index and split game from rest of string
+        key_split = line.find(':')
+        new_key = line[:key_split]
+        attempts = line[key_split + 1:]
+        # Parse rest of line into list of tuples
+        attempts = attempts.replace(';',',')
+        attempt_list = attempts.split(',')
+        split_vals = [tuple(attempt.strip().split(' ')) for attempt in attempt_list]
+        new_val = []
+        for pair in split_vals:
+            number = int(pair[0])
+            color = pair[1]
+            new_val.append((number,color))
+        data_dict[new_key] = new_val
+    return data_dict
 
-def verify_game_part_1():
-    pass
+def verify_game_part_1(game_list):
+    for game in game_list:
+        if game[0] > LIMITS[game[1]]:
+            return False
+    return True
+
+def convert_keys_to_int(data_dict):
+    new_dict = {}
+    for game in data_dict.keys():
+        new_key = int(game.strip('Game '))
+        new_dict[new_key] = data_dict[game]
+    return new_dict
 
 def main():
-    pass
+    data = retrieve_challenge_input()
+    data_dict = parse_data_part_1(data)
+    number_dict = convert_keys_to_int(data_dict)
+    results = [verify_game_part_1(game) for game in number_dict.values()]
+    results_dict = dict(zip(number_dict.keys(),results))
+    total = 0
+    for k in results_dict.keys():
+        if results_dict[k] == True:
+            total += k
+    return total
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print(main())
--- test_aoc/test_day_2.py
+++ test_aoc/test_day_2.py
@@ -1,9 +1,9 @@
 from challenges import day_2
 
-test_data = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n
+test_data = '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'''
 
 # Part 1 tests
@@ -14,16 +14,20 @@ def test_parse_part_1():
     result = day_2.parse_data_part_1(test_data)
     assert result["Game 1"] == [(3,"blue"),(4,"red"),(1,"red"),(2,"green"),(6,"blue"),(2,"green")]
 
+successful_list = [(3,"blue"),(4,"red"),(1,"red"),(2,"green"),(6,"blue"),(2,"green")]
+
 def test_successful_game():
     '''Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
     When verifying this game
     Then the function returns True'''
-    result = day_2.verify_game_part_1()
+    result = day_2.verify_game_part_1(successful_list)
     assert result == True
+
+failed_list = [(8,'green'),(6,'blue'),(20,'red'),(5,'blue'),(4,'red'),(13,'green'),(5,'green'),(1,'red')]
 
 def test_failed_game():
     '''Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
     When verifying this game
     Then the function returns False'''
-    result = day_2.verify_game_part_1()
+    result = day_2.verify_game_part_1(failed_list)
     assert result == False
