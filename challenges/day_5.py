87bbb49eb834caa01e9244e0c1011b7b523b41ae
Joseph Cancelliere <CancelliereJM@gmail.com>
2023-12-05 17:42:43 +0000

    Reworking tests part 1

--- challenges/day_5.py
+++ challenges/day_5.py
@@ -1,25 +1,92 @@
 from aocd.models import Puzzle
+import regex as re
 
 def retrieve_puzzle_input():
-    pass
+    puzzle = Puzzle(year=2023, day=5)
+    return puzzle.input_data
 
-def parse_puzzle_input():
-    pass
+def parse_puzzle_input(puzzle_input):
+    '''Convert the puzzle input from a single string into lists
+    Takes 1 arg, input string
+    Returns a Tuple:
+        - 1st element is list of seed numbers (int)
+        - 2nd element is a dict with maps for keys and lists of
+        tuples as values. Each tuple contains 2 integers for the mapping'''
+    # Create a list of seed numbers
+    seeds_index = puzzle_input.find("\n")
+    seed_line = puzzle_input[:seeds_index].strip("seeds: ")
+    seed_list = [int(seed) for seed in seed_line.split(" ")]
+    puzzle_input = puzzle_input[seeds_index:]
+    # Find the mapping names
+    pattern = ".+?:"
+    keys = re.findall(pattern,puzzle_input)
+    keys = [key.strip(" map:") for key in keys]
+    # Create a list of map lines for each map
+    map_list = []
+    next_map = puzzle_input.find(":")
+    while next_map > 0:
+        start = next_map + 2 # exclude colon & next newline
+        puzzle_input = puzzle_input[start:]
+        end_map = puzzle_input.find("\n\n")
+        map = puzzle_input[:end_map]
+        if end_map == -1: # Condition for last mapping in input
+            map = puzzle_input
+        lines = map.split("\n")
+        split_lines = [line.split(" ") for line in lines]
+        parsed_maps = [tuple([int(num) for num in map]) for map in split_lines]
+        map_list.append(parsed_maps)
+        next_map = puzzle_input.find(":")
+    # TODO: Combine keys and map_list into a singe dict.
+    map_dict = dict(zip(keys,map_list))
+    return (seed_list,map_dict)
 
-def make_mappings():
-    pass
+# Tried making a dict - input data was way too big.
+# def make_mappings(map_dict):
+#     '''Create a new dictionary for all the almanac mappings
+#     Returns a dict:
+#         - keys are the mapping destination
+#         - values are a dict containing mapping from source values
+#         to destination values'''
+#     map_keys = map_dict.keys()
+#     for key in map_keys:
+#         print(f"Values for {key}:")
+#         for i,line in enumerate(map_dict[key]):
+#             print(f"\nCompiling mapping {i+1}:")
+#             destination = line[0]
+#             source = line[1]
+#             range_len = line[2]
+#             print(f"\tSource start = {source}")
+#             print(f"\tDestination start = {destination}")
+#             print(f"\tRange = {range_len}")
+#             source_list = range(source,source+range_len)
+#             destination_list = range(destination,destination+range_len)
+#         break
 
-def convert_number():
-    pass
 
-def catalogue_seed_data():
+def convert_number(source_number,mapping):
+    '''generate an updated number based on the mapping provided
+    source_number is a singe int. mapping is the 3-tuple containing the source,
+    destination, and range of the mappings'''
+    destination = mapping[0]
+    source = mapping[1]
+    range_len = mapping[2]
+    if source_number < source:
+        return source_number
+    if source_number >= source:
+        source_diff = source_number - source
+        if source_diff >= range_len:
+            return source_diff + destination
+
+def catalogue_seed_data(seed_list):
     pass
 
 def get_lowest_loc():
     pass
 
 def main():
-    pass
+    puzzle_input = retrieve_puzzle_input()
+    seeds, maps = parse_puzzle_input(puzzle_input)
+    return convert_number(seeds[0],maps["seed-to-soil"][2])
 
 if __name__ == "__main__":
     print(main())
--- test_aoc/test_day_5.py
+++ test_aoc/test_day_5.py
@@ -44,100 +44,33 @@ humidity-to-location map:
 #   humidity 78         humidity 43        humidity 82         humidity 35
 #   location 82.        location 43.       location 86.        location 35.
 
-test_seeds, test_input_parsed = day_5.parse_puzzle_input()
+test_seeds, test_input_parsed = day_5.parse_puzzle_input(test_input)
 
-mappings = day_5.make_mappings()
+# Not making this function - real input data is too big
+# mappings = day_5.make_mappings(test_input_parsed)
 
-def test_convert_seed_soil():
-    # for seeds 79, 14, 55, 13
-    # when converting to soil
-    # then return 81, 14, 57, 13
-    result1 = day_5.convert_number(79,mappings["soil"])
-    assert result1 == 81
-    result2 = day_5.convert_number(14,mappings["soil"])
-    assert result2 == 14
-    result2 = day_5.convert_number(55,mappings["soil"])
-    assert result2 == 57
-    result4 = day_5.convert_number(13,mappings["soil"])
-    assert result4 == 13
+def test_below_start_mapping():
+    # given source number 14 and mapping (18, 25, 70)
+    # when converting the soure number
+    # return destination 14
+    result = day_5.convert_number(14,(18, 25, 70))
+    assert result == 14
 
-def test_convert_soil_fert():
-    # for soils 81, 14, 57, 13
-    # when converting to fertilizer
-    # then return 81, 53, 57, 52
-    result1 = day_5.convert_number(81,mappings["fertilizer"])
-    assert result1 == 81
-    result2 = day_5.convert_number(14,mappings["fertilizer"])
-    assert result2 == 53
-    result2 = day_5.convert_number(57,mappings["fertilizer"])
-    assert result2 == 57
-    result4 = day_5.convert_number(13,mappings["fertilizer"])
-    assert result4 == 52
 
-def test_convert_fert_water():
-    # for fertilizers 81, 53, 57, 52
-    # when converting to water
-    # then return 81, 49, 53, 41
-    result1 = day_5.convert_number(81,mappings["water"])
-    assert result1 == 81
-    result2 = day_5.convert_number(53,mappings["water"])
-    assert result2 == 49
-    result2 = day_5.convert_number(57,mappings["water"])
-    assert result2 == 53
-    result4 = day_5.convert_number(52,mappings["water"])
-    assert result4 == 41
 
-def test_convert_water_light():
-    # for waters 81, 49, 53, 41
-    # when converting to light
-    # then return 74, 42, 46, 34
-    result1 = day_5.convert_number(81,mappings["light"])
-    assert result1 == 74
-    result2 = day_5.convert_number(49,mappings["light"])
-    assert result2 == 42
-    result2 = day_5.convert_number(46,mappings["light"])
-    assert result2 == 46
-    result4 = day_5.convert_number(34,mappings["light"])
-    assert result4 == 34
+def test_above_range_mapping():
+    # given source number 81 and mapping (0, 11, 42)
+    # when converting the soure number
+    # return destination 81
+    result = day_5.convert_number(81,(0, 11, 42))
+    assert result == 81
 
-def test_convert_light_temp():
-    # for lights 74, 42, 46, 34
-    # when converting to temp
-    # then return 78, 42, 82, 34
-    result1 = day_5.convert_number(74,mappings["temp"])
-    assert result1 == 78
-    result2 = day_5.convert_number(42,mappings["temp"])
-    assert result2 == 42
-    result2 = day_5.convert_number(46,mappings["temp"])
-    assert result2 == 82
-    result4 = day_5.convert_number(34,mappings["temp"])
-    assert result4 == 34
-
-def test_convert_temp_humid():
-    # for temps 78, 42, 82, 34
-    # when converting to humidity
-    # then return 78, 43, 82, 35
-    result1 = day_5.convert_number(78,mappings["temp"])
-    assert result1 == 78
-    result2 = day_5.convert_number(42,mappings["temp"])
-    assert result2 == 43
-    result2 = day_5.convert_number(82,mappings["temp"])
-    assert result2 == 82
-    result4 = day_5.convert_number(34,mappings["temp"])
-    assert result4 == 35
-
-def test_convert_humid_loc():
-    # for temps 78, 43, 82, 35
-    # when converting to humidity
-    # then return 82, 43, 86, 35
-    result1 = day_5.convert_number(78,mappings["temp"])
-    assert result1 == 82
-    result2 = day_5.convert_number(43,mappings["temp"])
-    assert result2 == 43
-    result2 = day_5.convert_number(82,mappings["temp"])
-    assert result2 == 86
-    result4 = day_5.convert_number(35,mappings["temp"])
-    assert result4 == 35
+def test_within_range_mapping():
+    # given source number 81 and mapping (18, 25, 70)
+    # when converting the soure number
+    # return destination 74
+    result = day_5.convert_number(81,(18, 25, 70))
+    assert result == 74
 
 seed_catalogue = day_5.catalogue_seed_data(test_seeds)
 
