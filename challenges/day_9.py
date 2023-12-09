9732a01247a6797a8409dc7ca9f7874dcd39d91f
Joseph Cancelliere <CancelliereJM@gmail.com>
2023-12-09 07:30:29 +0000

    Write tests for day 9 part 1

---
+++ challenges/day_9.py
@@ -0,0 +1,17 @@
+from aocd.models import Puzzle
+
+def retreive_puzzle_input():
+    puzzle = Puzzle(year=2023, day=9)
+    return puzzle.input_data
+
+def parse_input():
+    pass
+
+def next_number(sequence):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    print(main())
---
+++ test_aoc/test_day_9.py
@@ -0,0 +1,19 @@
+from challenges import day_9
+
+test_input = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+# part 1 tests
+
+def test_next_value():
+    # Given the test input above
+    # When predicting the next number in the sequence
+    # return 18, 28, 68
+    parsed_test = day_9.parse_input(test_input)
+    result1 = day_9.next_number(test_input[0])
+    result2 = day_9.next_number(test_input[1])
+    result3 = day_9.next_number(test_input[2])
+    assert result1 == 18
+    assert result2 == 28
+    assert result3 == 68
