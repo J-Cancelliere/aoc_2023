4842cb932bcb592715ea85c59e86c251a286acef
Joseph Cancelliere <CancelliereJM@gmail.com>
2023-12-10 09:18:10 +0000

    Completed day 10 part 1

--- challenges/day_10.py
+++ challenges/day_10.py
@@ -1,16 +1,30 @@
 from aocd.models import Puzzle
+import time
+
+NAV_POINTS = {"|": ((-1,0), (1,0)),
+          "-": ((0,-1), (0,1)),
+          "F": ((1,0), (0,1)),
+          "7": ((0,-1), (1,0)),
+          "L": ((-1,0), (0,1)),
+          "J": ((0,-1), (-1,0))}
 
 def retrieve_puzzle_input():
     puzzle = Puzzle(year=2023, day=10)
     return puzzle.input_data
 
-def parse_grid():
-    pass
+def parse_grid(puzzle_input):
+    rows = puzzle_input.split("\n")
+    return rows
 
 # Find starting position, pick a direction
-def find_start():
-    pass
-
+def find_start(grid):
+    start = (0,0)
+    for i,line in enumerate(grid):
+        start_index = line.find("S")
+        if start_index != -1:
+            start = (i,start_index)
+            break
+    return start
 
 # Start following loop
 #   Loop navigation
@@ -18,16 +32,53 @@ def find_start():
 #   Determine next direction
 # Keep history of steps in loop
 # Return to start
-def follow_loop():
-    pass
+def follow_loop(grid,start):
+    r,c = start
+    history = []
+    looping = True
+    prev = (0,0)
+    while looping:
+        if history.count("S") == 2:
+            looping = False
+            continue
+        navigation = NAV_POINTS.get(grid[r][c])
+        # getting out of start position
+        if not navigation:
+            history.append("S")
+            search_list = [(r-1,c),(r+1,c),(r,c+1),(r,c-1)]
+            for point in search_list:
+                symbol = grid[point[0]][point[1]]
+                if symbol in NAV_POINTS.keys():
+                    for nav in NAV_POINTS[symbol]:
+                        if (point[0]+nav[0],point[1]+nav[1]) == (r,c):
+                            prev = (r,c)
+                            r,c = point
+                            break
+        # every other position
+        else:
+            for points in navigation:
+                next = (r + points[0], c + points[1])
+                if next == prev:
+                    continue
+                else:
+                    history.append(grid[r][c])
+                    prev = (r,c)
+                    r,c = next
+                    break
+    return {"steps": len(history), "history": history}
 
 # Divide steps by 2 (halfway point)
 # Return halfway point
-def halfway_point():
-    pass
+def halfway_point(steps):
+    return int(steps / 2)
 
 def main():
-    pass
+    puzzle = retrieve_puzzle_input()
+    grid = parse_grid(puzzle)
+    start = find_start(grid)
+    history = follow_loop(grid,start)
+    part1 = halfway_point(history["steps"])
+    return f"Answer to part 1: {part1}"
 
 if __name__ == "__main__":
     print(main())
--- test_aoc/test_day_10.py
+++ test_aoc/test_day_10.py
@@ -22,9 +22,9 @@ def test_starting_position():
     grid2 = day_10.parse_grid(test_input_2)
     result1 = day_10.find_start(grid1)
     result2 = day_10.find_start(grid2)
-    assert result1[0] == 0
+    assert result1[0] == 1
     assert result1[1] == 1
-    assert result2[0] == 1
+    assert result2[0] == 2
     assert result2[1] == 0
 
 def test_history_start_finish():
@@ -35,8 +35,8 @@ def test_history_start_finish():
     grid2 = day_10.parse_grid(test_input_2)
     start1 = day_10.find_start(grid1)
     start2 = day_10.find_start(grid2)
-    result1 = day_10.follow_loop(start1)
-    result2 = day_10.follow_loop(start2)
+    result1 = day_10.follow_loop(grid1,start1)
+    result2 = day_10.follow_loop(grid2,start2)
     assert result1["history"][0] == "S"
     assert result1["history"][-1] == "S"
     assert result2["history"][0] == "S"
@@ -45,15 +45,15 @@ def test_history_start_finish():
 def test_step_counter():
     # given test inputs 1 and 2
     # when following the loops
-    # returned step counts should 8 and 16 respectively
+    # returned step counts should 9 and 17 respectively
     grid1 = day_10.parse_grid(test_input_1)
     grid2 = day_10.parse_grid(test_input_2)
     start1 = day_10.find_start(grid1)
     start2 = day_10.find_start(grid2)
-    result1 = day_10.follow_loop(start1)
-    result2 = day_10.follow_loop(start2)
-    assert result1["steps"][0] == 8
-    assert result2["steps"][0] == 16
+    result1 = day_10.follow_loop(grid1,start1)
+    result2 = day_10.follow_loop(grid2,start2)
+    assert result1["steps"] == 9
+    assert result2["steps"] == 17
 
 def test_halfway_value():
     # given test inputs 1 and 2
@@ -63,8 +63,8 @@ def test_halfway_value():
     grid2 = day_10.parse_grid(test_input_2)
     start1 = day_10.find_start(grid1)
     start2 = day_10.find_start(grid2)
-    history1 = day_10.follow_loop(start1)
-    history2 = day_10.follow_loop(start2)
+    history1 = day_10.follow_loop(grid1,start1)
+    history2 = day_10.follow_loop(grid2,start2)
     result1 = day_10.halfway_point(history1["steps"])
     result2 = day_10.halfway_point(history2["steps"])
     assert result1 == 4
